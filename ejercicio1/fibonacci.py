e0757d3108e0b6f8dc24e0f094342c6eaf08c8c5
ericitstep <133179695+ericitstep@users.noreply.github.com>
2023-06-17 00:18:32 -0700

    Add files via upload

--- ejercicio1/fibonacci.py
+++ ejercicio1/fibonacci.py
@@ -1,13 +1,15 @@
+# Códifique una función que usando la técnica de recursividad imprima el
+# enesimo elemento de la serie de fibonacci.
+
+# Calcula y devuelve el n-ésimo elemento de la serie de Fibonacci.
+
+    #  Argumentos:
+    #      n (int): El índice del elemento a recuperar.
+
+    #  Devoluciones:
+    #      int: El valor del n-ésimo elemento de la serie de Fibonacci.
 def fibonacci(n):
-    """
-    Calcula y devuelve el n-ésimo elemento de la serie de Fibonacci.
-
-     Argumentos:
-         n (int): El índice del elemento a recuperar.
-
-     Devoluciones:
-         int: El valor del n-ésimo elemento de la serie de Fibonacci.
-    """
+    
     if n == 0:
         return 0
     elif n == 1:
--- ejercicio2/cuadrado_entero.py
+++ ejercicio2/cuadrado_entero.py
@@ -1,16 +1,20 @@
+# Códifique un algoritmo en python que tome como argumento un valor
+# flotante, obtenga su cuadrado y lo convierta a entero usando la técnica de
+# composición.
+
+# Calcula el cuadrado de un valor flotante y lo convierte a entero usando la técnica de composición.
+
+    # Args:
+    #     valor (float): El valor flotante al que se le calculará el cuadrado y se convertirá a entero.
+
+    # Retorna:
+    #     int: El cuadrado del valor como un entero.
+
+    # Ejemplo:
+    # cuadrado_entero(2.5)
+
 def cuadrado_entero(valor):
-    """
-    Calcula el cuadrado de un valor flotante y lo convierte a entero usando la técnica de composición.
-
-    Args:
-        valor (float): El valor flotante al que se le calculará el cuadrado y se convertirá a entero.
-
-    Retorna:
-        int: El cuadrado del valor como un entero.
-
-    Ejemplo:
-    cuadrado_entero(2.5)
-    """
+     
     cuadrado = valor ** 2  # Calcula el cuadrado del valor
     entero = int(cuadrado)  # Convierte el cuadrado a entero usando la técnica de composición
 
